c3aaf270116351fb55f751d2a0d13ec89a27d35a
Sudhir Kumar <52371851+sudhir-kumar-au3@users.noreply.github.com>
2020-11-21 10:49:42 +0530

    prettier fix

--- src/components/FirstComponent.js
+++ src/components/FirstComponent.js
@@ -4,19 +4,19 @@ import { Typography } from "@material-ui/core";
 import PropTypes from "prop-types";
 import AdbIcon from "@material-ui/icons/Adb";
 const FirstComponent = ({ name }) => {
-    return (
-        <React.Fragment>
-            <div style={{ margin: "auto" }} className="main">
-                {name}
-                <Typography variant="h1" align="center" color="error">
-                    {name}
-                </Typography>
-                <AdbIcon></AdbIcon>
-            </div>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <div style={{ margin: "auto" }} className="main">
+        {name}
+        <Typography variant="h1" align="center" color="error">
+          {name}
+        </Typography>
+        <AdbIcon></AdbIcon>
+      </div>
+    </React.Fragment>
+  );
 };
 FirstComponent.propTypes = {
-    name: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
 };
 export default FirstComponent;
--- webpack.config.js
+++ webpack.config.js
@@ -3,109 +3,104 @@ const pkg = require("./package.json");
 const path = require("path");
 const libraryName = pkg.name;
 module.exports = {
-    entry: path.join(__dirname, "./src/index.js"),
-    output: {
-        path: path.join(__dirname, "./dist"),
-        filename: "[name].js",
-        chunkFilename: "[id].[chunkhash].js",
-        library: libraryName,
-        libraryTarget: "umd",
-        publicPath: "/dist/",
-        umdNamedDefine: true,
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "[name].css",
-            chunkFilename: "[id].css",
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    {
-                        loader: "url-loader",
-                        options: {
-                            fallback: "file-loader",
-                            name: "[name][md5:hash].[ext]",
-                            outputPath: "assets/",
-                            publicPath: "/assets/",
-                        },
-                    },
-                ],
+  entry: path.join(__dirname, "./src/index.js"),
+  output: {
+    path: path.join(__dirname, "./dist"),
+    filename: "[name].js",
+    chunkFilename: "[id].[chunkhash].js",
+    library: libraryName,
+    libraryTarget: "umd",
+    publicPath: "/dist/",
+    umdNamedDefine: true,
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              fallback: "file-loader",
+              name: "[name][md5:hash].[ext]",
+              outputPath: "assets/",
+              publicPath: "/assets/",
             },
-            {
-                test: /\.*css$/i,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            publicPath: (resourcePath, context) => {
-                                return (
-                                    path.relative(
-                                        path.dirname(resourcePath),
-                                        context
-                                    ) + "/"
-                                );
-                            },
-                        },
-                    },
-                    "css-loader",
-                ],
-            },
-            {
-                test: /\.(js|jsx)$/,
-                use: ["babel-loader"],
-                include: path.resolve(__dirname, "src"),
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.(eot|ttf|woff|woff2)$/,
-                use: ["file-loader"],
-            },
-            {
-                test: /\.(pdf|doc|zip)$/,
-                use: ["file-loader"],
-            },
+          },
         ],
-    },
-    resolve: {
-        alias: {
-            react: path.resolve(__dirname, "./node_modules/react"),
-            "react-dom": path.resolve(__dirname, "./node_modules/react-dom"),
-            assets: path.resolve(__dirname, "assets"),
-            "@material-ui/core": path.resolve(
-                __dirname,
-                "./node_modules/@material-ui/core"
-            ),
-            "@material-ui/icons": path.resolve(
-                __dirname,
-                "./node_modules/@material-ui/icons"
-            ),
-            "@material-ui/styles": path.resolve(
-                __dirname,
-                "./node_modules/@material-ui/styles"
-            ),
-        },
-    },
-    externals: [
-        {
-            // Don't bundle react or react-dom
-            react: {
-                commonjs: "react",
-                commonjs2: "react",
-                amd: "React",
-                root: "React",
+      },
+      {
+        test: /\.*css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: (resourcePath, context) => {
+                return path.relative(path.dirname(resourcePath), context) + "/";
+              },
             },
-            "react-dom": {
-                commonjs: "react-dom",
-                commonjs2: "react-dom",
-                amd: "ReactDOM",
-                root: "ReactDOM",
-            },
-        },
-        // excluding material ui from bundle
-        /@material-ui\/.*/,
+          },
+          "css-loader",
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        use: ["babel-loader"],
+        include: path.resolve(__dirname, "src"),
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.(pdf|doc|zip)$/,
+        use: ["file-loader"],
+      },
     ],
+  },
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, "./node_modules/react"),
+      "react-dom": path.resolve(__dirname, "./node_modules/react-dom"),
+      assets: path.resolve(__dirname, "assets"),
+      "@material-ui/core": path.resolve(
+        __dirname,
+        "./node_modules/@material-ui/core"
+      ),
+      "@material-ui/icons": path.resolve(
+        __dirname,
+        "./node_modules/@material-ui/icons"
+      ),
+      "@material-ui/styles": path.resolve(
+        __dirname,
+        "./node_modules/@material-ui/styles"
+      ),
+    },
+  },
+  externals: [
+    {
+      // Don't bundle react or react-dom
+      react: {
+        commonjs: "react",
+        commonjs2: "react",
+        amd: "React",
+        root: "React",
+      },
+      "react-dom": {
+        commonjs: "react-dom",
+        commonjs2: "react-dom",
+        amd: "ReactDOM",
+        root: "ReactDOM",
+      },
+    },
+    // excluding material ui from bundle
+    /@material-ui\/.*/,
+  ],
 };
